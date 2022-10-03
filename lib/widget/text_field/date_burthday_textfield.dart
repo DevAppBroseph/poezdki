8197f9e879301947f50dad338e1dbfb7ac007744
Hashishvilli <44935349+OlegBochkoV@users.noreply.github.com>
2022-10-03 20:38:41 +0300

    editcontroller dob

--- lib/widget/text_field/date_burthday_textfield.dart
+++ lib/widget/text_field/date_burthday_textfield.dart
@@ -1,7 +1,9 @@
+import 'dart:io';
 import 'package:app_poezdka/const/colors.dart';
 import 'package:app_poezdka/util/validation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:keyboard_actions/keyboard_actions.dart';
 
 class DateBirthdayTextField extends StatefulWidget {
   final Function? onTap;
@@ -52,10 +54,95 @@ class DateBirthdayTextField extends StatefulWidget {
 
 class _DateBirthdayTextFieldState extends State<DateBirthdayTextField> {
 
+  final FocusNode nodeText = FocusNode();
+  Color textFieldBorderColor = kPrimaryWhite;
+  Color hintTextColor = Colors.grey;
   String text = '';
 
   @override
   Widget build(BuildContext context) {
+    if(Platform.isIOS) {
+      return KeyboardActions(
+          config: KeyboardActionsConfig(actions: [
+            KeyboardActionsItem(
+              focusNode: nodeText,
+              onTapAction: () => nodeText.unfocus(),
+            ),
+          ]),
+          child: TextFormField(
+            controller: widget.dob,
+              validator: Validations.validateDateBirthday,
+              textInputAction: TextInputAction.done,
+              maxLines: 1,
+              minLines: 1,
+          focusNode: nodeText,
+          onChanged: (value) {
+                if(value[value.length-1] != '.') {
+                  if(value.length >= text.length) {
+                    if(value.length == 2 || value.length == 5) {
+                      widget.dob.text = value + '.';
+                      widget.dob.selection = TextSelection.fromPosition(TextPosition(offset: widget.dob.text.length));
+                    } else {
+                      if(value.length == 3 || value.length == 6) {
+                        widget.dob.text = text + '.' + value[value.length-1];
+                        widget.dob.selection = TextSelection.fromPosition(TextPosition(offset: widget.dob.text.length));
+                      }
+                    }
+                  }
+                }
+                text = widget.dob.text;
+              },
+          cursorColor: kPrimaryColor,
+          inputFormatters: [LengthLimitingTextInputFormatter(10)],
+          keyboardType: TextInputType.number,
+          style: const TextStyle(color: Colors.black, overflow: TextOverflow.ellipsis),
+          decoration: InputDecoration(
+                  suffixStyle: const TextStyle(
+                    overflow: TextOverflow.ellipsis,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
+                  ),
+                  errorStyle: const TextStyle(fontSize: 10.0),
+                  contentPadding:
+                      const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                  filled: true,
+                  fillColor: kPrimaryWhite,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(
+                      color: kPrimaryWhite,
+                      width: 0.0,
+                      style: BorderStyle.solid,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(
+                      color: kPrimaryWhite,
+                      width: 0.0,
+                      style: BorderStyle.solid,
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(
+                      color: kPrimaryWhite,
+                      width: 0.0,
+                      style: BorderStyle.solid,
+                    ),
+                  ),
+                  hintStyle: const TextStyle(
+                    overflow: TextOverflow.ellipsis,
+                    fontSize: 14,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.w400,
+                  ),
+                  hintText: widget.hintText,
+                  ),
+        ),
+      );
+    }
+    
     return TextFormField(
               onChanged: (value) {
                 if(value[value.length-1] != '.') {
